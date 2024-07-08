8a9c67acd7de9fb8b6ca42cb3bbbce0e5cb59d10
MorsHs <77497581+MorsHs@users.noreply.github.com>
2024-07-08 21:55:03 +0800

    Delete flutter_maps_taripa directory

--- flutter_maps_taripa/android/app/src/main/kotlin/com/example/flutter_maps_taripa/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_maps_taripa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_maps_taripa/lib/Shards/locationList.dart
+++
@@ -1,6 +0,0 @@
-class Location {
-  final String location;
-  final String distance;
-
-  Location({required this.location, required this.distance});
-}
