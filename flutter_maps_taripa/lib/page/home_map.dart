9388c07c37dbb6c275511ac7b678f4551ddf4079
MorsHs <77497581+MorsHs@users.noreply.github.com>
2024-07-08 21:54:26 +0800

    Upload Taripa
    code for the taripa added

---
+++ flutter_maps_taripa/lib/page/home_map.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_maps_taripa/controller_animation/map.dart';
+
+class HomeMap extends StatelessWidget {
+  const HomeMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MapView();
+  }
+}
